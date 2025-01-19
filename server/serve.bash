db32b0674d779701df274d9f82fbc80eb91d3ad2
Tyler <tyler.wayne2002@hotmail.com>
2025-01-19 12:42:20 -0600

    back to 3B

--- server/serve.bash
+++ server/serve.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
-export INFERENCE_MODEL="meta-llama/Llama-3.2-1B-Instruct"
+export INFERENCE_MODEL="meta-llama/Llama-3.2-3B-Instruct"
 # ollama names this model differently, and we must use the ollama name when loading the model
-export OLLAMA_INFERENCE_MODEL="llama3.2:1b-instruct-fp16"
+export OLLAMA_INFERENCE_MODEL="llama3.2:3b-instruct-fp16"
 export LLAMA_STACK_PORT=5001
 
 # Find IP Address
