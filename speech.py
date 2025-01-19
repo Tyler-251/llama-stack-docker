679ef6afd8622382bca0bbb732cc66c3f609ec89
Tyler Russell <82682594+Tyler-251@users.noreply.github.com>
2025-01-19 11:12:01 -0600

    reorg ignored files

--- speech.py
+++
@@ -1,7 +0,0 @@
-import pyautogui
-import os
-os.environ["PYAUTOGUI_OS"] = "windows"
-
-
-screenshot = pyautogui.screenshot()
-screenshot.save('./temp/screenshots/screenshot.png')
--- tesstest.py
+++
@@ -1,6 +0,0 @@
-import pytesseract
-
-# https://pypi.org/project/pytesseract/
-pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
-print(pytesseract.image_to_data('./temp/screenshots/screenshot.png'))
-
